c286881e92eb50e0a9bf1f5f0edbac60e2f5ebba
Kemunto Maragia <kmaragia@email.wm.edu>
2019-01-21 03:25:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
+  p = ["a", "b","c", "d"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  p.push("e")
   
 end
 
 def add_element_to_start_of_array(array, element)
+  p.unshift("s")
   
 end
 
 def remove_element_from_end_of_array(array)
+  p.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  p.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  h = [p,0]
   
 end
 
 def retrieve_first_element_from_array(array)
+  f = [p,0]
   
 end
 
 def retrieve_last_element_from_array(array)
+  
   
 end
